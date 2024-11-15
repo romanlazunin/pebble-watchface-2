1896eded450e5e4e22e50108d97dc33c9a626874
Roman Lazunin <rlazunin@gmail.com>
2024-11-15 22:22:32 +0000

    centered time text vertically

--- src/c/watchface-2.c
+++ src/c/watchface-2.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 
 static Window *s_window;
+static TextLayer *s_background_layer;
 static TextLayer *s_time_layer;
 
 static void update_time() {
@@ -25,14 +26,17 @@ static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+  s_background_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));  
 
-  s_time_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  text_layer_set_background_color(s_background_layer, GColorBlack);
 
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorRed);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_background_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
